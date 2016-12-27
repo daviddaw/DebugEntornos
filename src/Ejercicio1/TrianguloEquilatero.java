587cfef9f062cb2b37a8ef7076f14af6f2ada5db
daviddaw <kilimanjaro_10>
2016-12-27 22:16:36 +0100

    clase triangulo creada sin operaciones especificas

--- src/Ejercicio1/TrianguloEquilatero.java
+++ src/Ejercicio1/TrianguloEquilatero.java
@@ -1,8 +1,34 @@
 package Ejercicio1;
 
 public class TrianguloEquilatero {
-private void syso() {
-	// TODO Auto-generated method stub
+private double lado;
+
+public TrianguloEquilatero(double lado) {
+	this.lado = lado;
+}
+
+public TrianguloEquilatero() {
+	
+}
+
+public double getLado() {
+	return lado;
+}
+
+public void setLado(double lado) {
+	this.lado = lado;
+}
+
+public double getAltura() {
+	return lado;
+
+}
+public double getArea() {
+	return lado;
+
+}
+public double getPerimetro() {
+	return lado;
 
 }
 }
