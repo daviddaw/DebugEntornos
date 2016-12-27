93e608e0dd2095825d6fa93c28abfbd8e8c66970
daviddaw <kilimanjaro_10>
2016-12-27 22:44:46 +0100

    constructor con tres campos

---
+++ src/ejercicio2/Articulo.java
@@ -0,0 +1,27 @@
+package ejercicio2;
+
+public class Articulo {
+private  int codigo;
+public String nombre;
+private String color;
+private double precio;
+
+public Articulo(String nombre, String color, double precio) {
+	int contador=0;
+	this.nombre = nombre;
+	this.color = color;
+	this.precio = precio;
+	contador=contador++;
+	codigo=contador;
+}
+
+
+
+
+
+
+
+
+
+
+}
