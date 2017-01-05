9f2f599a0df5b6c5cab977e3903ef88ca9bf4f3a
daviddaw <kilimanjaro_10>
2017-01-05 11:26:01 +0100

    listo a empezar

---
+++ src/parte1/Ejercicio01.java
@@ -0,0 +1,15 @@
+package parte1;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		// aceptar numeros hastaque la suma sea supeior a 1000
+		int numero,acumulador=0;		
+	System.out.println("Escribe números por teclado  hasta que la suma sea superior a 1000");
+		do {
+			numero=LeerTeclado.readInteger();
+			acumulador=acumulador+numero;
+		} while (acumulador<=1000);
+System.out.println("La suma fué "+acumulador);
+	}
+}
